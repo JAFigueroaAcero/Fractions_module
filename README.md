6de3f48fb2ae51151667fc83f61f67256feba70f
JAFigueroaAcero <98606608+JAFigueroaAcero@users.noreply.github.com>
2022-05-20 16:10:46 -0500

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Fractions_module
- Module to use fraction operators in python
+Version 1.0.0
+Module to use fraction operators in python.
+According to the next sintaxis 'a |op| b', the existing operators are
+- |f|: to convert to values to frac array.
+- |s|: to sum 2 fraction arrays.
+- |r|: to rest 2 fraction arrays.
+- |x|: to multiply 2 fraction arrays.
+- |d|: to divide 2 fraction arrays.
+Also there are two operators with the sintaxis 'a |op':
+- |sim: simplify a fraction array.
+- |dec: converts a fraction array to float variable.
+
---
+++ fractions.py
@@ -0,0 +1,76 @@
+'''
+Module to use fraction operators without calling functions directly
+
+--To operate use 'a |op| b' or 'a |sim' and 'a |dec' to simplificate or convert a fraction --
+
+'''
+
+class Infix:
+    def __init__(self, function):
+        self.function = function
+    def __ror__(self, other):
+        return Infix(lambda x, self=self, other=other: self.function(other, x))
+    def __or__(self, other):
+        return self.function(other)
+
+class Infix_i:
+    def __init__(self, function):
+        self.function = function
+    def __ror__(self, other):
+        return self.function(other)
+
+def frac(x,y):
+    return [x,y]|sim
+
+def sum_frac(x,y):
+    a = x[0]*y[1] + y[0]*x[1]
+    b = y[1]*x[1]
+    return [a,b]|sim
+
+def res_frac(x,y):
+    a = x[0]*y[1] - y[0]*x[1]
+    b = y[1]*x[1]
+    return [a,b]|sim
+
+def mult_frac(x,y):
+    return [x[0]*y[0],x[1]*y[1]]|sim
+
+def div_frac(x,y):
+    return [x[0]*y[1],x[1]*y[0]]|sim
+
+def sim_frac(a):
+    new_a = [[],[]]
+    for m,n in enumerate(a):
+        act = n
+        new_a[m].append(0 if a[m] == 0 else 1)
+        while act != 1:
+            v = 2
+            while v < act/2 + 1:
+                if act % v == 0:
+                    new_a[m].append(v)
+                    act = act // v 
+                    break
+                else:
+                    v += 1
+            else:
+                new_a[m].append(act)
+                break
+    for f in new_a[0][1::]:
+        if f in new_a[1]:
+            new_a[0].pop(new_a[0].index(f))
+            new_a[1].pop(new_a[1].index(f))
+    new_am = [1,1]
+    for rep,a in enumerate(new_a):
+        for n in a:
+            new_am[rep] *= n
+    return new_am
+def dec(a):
+    return a[0]/a[1]
+
+f = Infix(lambda x,y: frac(x,y))       # Do frac
+sim = Infix_i(lambda x: sim_frac(x))  # Simplificate frac
+s = Infix(lambda x,y: sum_frac(x,y))  # Sum frac
+r = Infix(lambda x,y: res_frac(x,y))  # Substract frac
+x = Infix(lambda x,y: mult_frac(x,y)) # Multiplicate frac
+d = Infix(lambda x,y: div_frac(x,y))  # Divide frac
+dec = Infix_i(lambda x: dec(x))       # Converts to float
